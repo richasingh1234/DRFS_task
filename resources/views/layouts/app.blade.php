ba8348cb9e21c401965bf59366a5baf2f7c34269
richasingh1234 <rrichasingh84@gmail.com>
2020-09-20 23:52:44 +0530

    logo upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
         <div class="" style="background-color: white;">
             <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
                 <h5 class="my-0 mr-md-auto font-weight-normal">
-                    <img src="{{ asset('assets/web_images/logo.png') }}" class="rounded-circle mx-auto" width="50px">
+                    <img src="{{ asset('assets/web_images/logo_pro.png') }}" class="rounded-circle mx-auto" width="50px">
                 </h5>
                 <nav class="my-2 my-md-0 mr-md-3">
                     <a class="p-2 text-dark" href="{{ route('dashboard') }}"><i class="fa fa-home"></i> Home</a>
